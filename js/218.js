ca7a5f1b0a7b3d1a3889179681f7bee87bec55fd
Xyan Xue <ellenxue2001@gmail.com>
2024-03-06 14:10:09 -0700

    1

---
+++ js/218.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const accordions = document.getElementsByClassName('accordion-button');
+  
+    for (let i = 0; i < accordions.length; i++) {
+      accordions[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + 'px';
+        } 
+      });
+    }
+  });
+  
