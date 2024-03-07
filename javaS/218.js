9621e8114220cfeddb6dd7e25e2ca6723de6d8bb
Xyan Xue <ellenxue2001@gmail.com>
2024-03-06 19:32:02 -0700

    风琴

--- javaS/218.js
+++ javaS/218.js
@@ -1,25 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Get all the elements with the class "accordion-button"
-  var acc = document.getElementsByClassName("accordion-button");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-          // Toggle between adding and removing the "active" class to highlight the button that controls the panel
-          this.classList.toggle("active");
-
-          // Select the next element sibling (which should be the .accordion-content div)
-          var panel = this.nextElementSibling;
-
-          // Toggle between hiding and showing the active panel
-          if (panel.style.maxHeight) {
-              // Panel is open, needs to be closed
-              panel.style.maxHeight = null;
-          } else {
-              // Panel is closed, needs to be open
-              // This opens the panel (using maxHeight can provide a smooth animation)
-              panel.style.maxHeight = panel.scrollHeight + "px";
-          } 
-      });
-  }
-});
+var acc = document.getElementsByClassName("accordion-button");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
